50e414094139e03b390514d986343f7288c65f93
Dominic Gerth <d.gerth1990@googlemail.com>
2022-04-06 21:13:30 +0200

    addet routing and components for user and dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
