4b73b1649b66fdc83d4e4366226426bad0b8060f
Dominic Gerth <d.gerth1990@googlemail.com>
2022-04-07 19:11:27 +0200

    addet user table and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -52,6 +54,8 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
     AngularFireModule,
     AngularFirestoreModule,
     AngularFireModule.initializeApp(environment.firebase),
+    MatProgressBarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/models/user.class';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -12,21 +13,24 @@ export class DialogAddUserComponent implements OnInit {
 
   user: User = new User();
   birthDate!: Date;
-  constructor(private firestore: AngularFirestore) { }
+  loading = false;
+
+  constructor(public dialogRef: MatDialogRef<DialogAddUserComponent>, private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
   }
 
   save() {
+    this.loading = true;
     this.user.birthDate = this.birthDate.getTime();
-    // console.log(this.user)
-
     this.firestore
-    .collection('users')
-    .add(this.user.toJSON())
-    .then((result: any)=>{
-      console.log('adding user finished', result)
-    })
+      .collection('users')
+      .add(this.user.toJSON())
+      .then((result: any) => {
+        console.log('adding user finished', result);
+        this.dialogRef.close();
+        this.loading = false;
+      })
   }
 
 }
